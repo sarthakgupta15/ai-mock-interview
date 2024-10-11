2636aac50b314652fcbb47d6ae48c70866ab433b
sarthakgupta15 <sarthakgupta.univ@gmail.com>
2024-10-11 10:17:54 +0530

    Project Setup & Login

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <div>
+        <h2>Hi, Sarthak here! </h2>
+        <Button>Buy Me a coffee</Button>
+      </div>
+  );
+}
