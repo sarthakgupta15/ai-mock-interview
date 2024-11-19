cbe6c6f8e4bc7c11816b9bd04225b9666b39688c
Sarthak Gupta <sarthakgupta@Snigdhas-MacBook-Air.local>
2024-11-19 15:04:19 +0530

    AI feedback & Record in db

--- app/dashboard/interview/[interviewId]/start/page.jsx
+++ app/dashboard/interview/[interviewId]/start/page.jsx
@@ -5,10 +5,12 @@ import { eq } from 'drizzle-orm';
 import React, { useEffect, useState } from 'react'
 import QuestionsSection from './_components/QuestionsSection';
 import RecordAnswerSection from './_components/RecordAnswerSection';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 function StartInterview({params}) {
 
-    const [inteviewData, setInterviewData] = useState();
+    const [interviewData, setInterviewData] = useState();
     const [mockInterviewQuestion, setMockInterviewQuestion] = useState();
     const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
 
@@ -43,7 +45,19 @@ function StartInterview({params}) {
         <RecordAnswerSection
         mockInterviewQuestion = {mockInterviewQuestion}
         activeQuestionIndex = {activeQuestionIndex}
+        interviewData = {interviewData}
         />
+      </div>
+      <div className='flex justify-end gap-6'>
+        {activeQuestionIndex>0 && 
+        <Button onClick={()=> setActiveQuestionIndex(activeQuestionIndex-1)} >Previous Question</Button> }
+        {activeQuestionIndex!=mockInterviewQuestion?.length-1 && 
+        <Button onClick={()=> setActiveQuestionIndex(activeQuestionIndex+1)} >Next Question</Button> }
+        {activeQuestionIndex==mockInterviewQuestion?.length-1 && 
+        <Link href={'/dashboard/interview/' + interviewData?.mockId+"/feedback"}>
+        
+        <Button>End Interview</Button> 
+        </Link>}
       </div>
     </div>
   )
