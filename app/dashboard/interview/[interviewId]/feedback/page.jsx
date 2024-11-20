ae103e9dc60f22c85fd105b9aa12072dcc7b8827
Sarthak Gupta <sarthakgupta@Snigdhas-MacBook-Air.local>
2024-11-20 14:03:26 +0530

    Previous Interview Cards

--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -35,6 +35,11 @@ function Feedback({params}) {
 
   return (
     <div className='p-10'>
+      
+      {feedbackList?.length==0?
+        <h2 className='font-bold text-xl text-gray-500'>No Interview Feedback Record Found</h2>  
+            :
+        <>
       <h2 className='text-3xl font-bold text-green-500'>Congratulations</h2>
       <h2 className='font-bold text-2xl'>Here is your interview Feedback</h2>
       <h2 className='text-primary text-lg my-3'>Your overall interview rating is 7/10</h2>
@@ -51,12 +56,13 @@ function Feedback({params}) {
           <div className='flex flex-col gap-2'>
             <h2 className='text-red-500 p-2 border rounded-lg'><strong>Rating: </strong>{item.rating}</h2>
             <h2 className='p-2 border rounded-lg bg-red-50 text-sm text-red-900'><strong>Your Answer: </strong>{item.userAns}</h2>
-            <h2 className='p-2 border rounded-lg bg-green-50 text-sm text-green-900'><strong>Correct Answer: </strong>{item.correctAns}</h2>
+            {/* <h2 className='p-2 border rounded-lg bg-green-50 text-sm text-green-900'><strong>Correct Answer: </strong>{item.correctAns}</h2> */}
             <h2 className='p-2 border rounded-lg bg-blue-50 text-sm text-primary-900'><strong>Feedback: </strong>{item.feedback}</h2>
           </div>
         </CollapsibleContent>
         </Collapsible>
       ))}
+      </>}
 
       <Button onClick={()=> router.replace('/dashboard')}>Go Home</Button>
     </div>
